e90eb077b56ae1fd1853af901e8084aa27db49ac
nizar-maatoug <nizar.maatoug12@gmail.com>
2023-01-26 08:44:44 +0100

    Version initiale

---
+++ src/main/java/edu/isetbizerte/spring2023eventssportif/Spring2023EventsSportifApplication.java
@@ -0,0 +1,13 @@
+package edu.isetbizerte.spring2023eventssportif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2023EventsSportifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2023EventsSportifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/isetbizerte/spring2023eventssportif/Spring2023EventsSportifApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.isetbizerte.spring2023eventssportif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2023EventsSportifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
